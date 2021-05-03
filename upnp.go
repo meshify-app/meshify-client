a5dddcc78527aa2d3b7354483ff9319002483850
Alan Graham <alan@meshify.app>
2021-05-02 22:59:32 -0700

    add logging

--- upnp.go
+++ upnp.go
@@ -3,6 +3,7 @@ package main
 import (
 	upnp "github.com/meshify-app/go-upnp"
 	"github.com/meshify-app/meshify/model"
+	log "github.com/sirupsen/logrus"
 )
 
 func ConfigureUPnP(host model.Host) error {
@@ -11,12 +12,16 @@ func ConfigureUPnP(host model.Host) error {
 		router, err := upnp.Discover()
 
 		if err != nil {
+			log.Error("Error discovering gateway, upnp likely not supported. %v", err)
 			return err
 		}
 
-		if host.Current.ListenPort != 0 {
+		if host.Current.ListenPort != 0 && host.Current.Endpoint != "" {
 			router.Clear(uint16(host.Current.ListenPort))
-			router.ForwardUDP(uint16(host.Current.ListenPort), host.Name+" "+host.MeshName)
+			err = router.ForwardUDP(uint16(host.Current.ListenPort), host.Name+" "+host.MeshName)
+			if err != nil {
+				log.Errorf("Error configuring port forward for %d on host%s for mesh %s.  %v", host.Current.ListenPort, host.Name, host.MeshName, err)
+			}
 		}
 	}
 
